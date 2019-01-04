0b40a5c16aee855b4ae8205531a8f56d4366d468
kapil <kapil@kapilsingh.local>
2019-01-04 14:06:40 +0530

    code with dockerfile

---
+++ build/precache-manifest.6b03157041e7166690a4569c75f02957.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "51dfceb4cded0b7a3cd2",
+    "url": "/static/js/main.51dfceb4.chunk.js"
+  },
+  {
+    "revision": "fa92c112869d3657d47b",
+    "url": "/static/js/1.fa92c112.chunk.js"
+  },
+  {
+    "revision": "51dfceb4cded0b7a3cd2",
+    "url": "/static/css/main.c1f34a8d.chunk.css"
+  },
+  {
+    "revision": "18dd8323fcf2b5d2f0dfedb5fb19e53f",
+    "url": "/index.html"
+  }
+];
